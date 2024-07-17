4aeab6f71b8c000b26c823e9c7db5607c0a869fc
Visal Vishmitha Alwis <106392603+vishmitha1@users.noreply.github.com>
2024-07-17 22:56:13 +0530

    init

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,28 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-private float speed;
-private float jumpForce;
-private float movementHorizontal;
-private Rigidbody2D rb2D;
-
-
 {
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    private float movementHorizontal;
+    private Rigidbody2D rb2D;
+    private bool isGrounded;
+    [SerializeField] private Transform groundCheck;  // Make sure to assign this in the Inspector
+    [SerializeField] private float checkRadius;
+    [SerializeField] private LayerMask whatIsGround;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        movementHorizontal = Input.GetAxis("Horizontal");
-
+        getInput();
     }
 
-    private void fixedUpdate(){
-        
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position,checkRadius, whatIsGround);
+        movePlayer();
+    }
+
+    private void getInput()
+    {
+        movementHorizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetAxis("Jump") > 0 && isGrounded)
+        {
+            rb2D.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void movePlayer()
+    {
+        rb2D.velocity = new Vector2(movementHorizontal * speed, rb2D.velocity.y);
     }
 }
