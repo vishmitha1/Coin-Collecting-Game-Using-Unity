06270da1d9b323dd83662ef0b982ca00a1a8512c
Visal Vishmitha Alwis <106392603+vishmitha1@users.noreply.github.com>
2024-07-19 19:29:00 +0530

    Updated

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,9 +13,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float checkRadius;
     [SerializeField] private LayerMask whatIsGround;
 
+    private bool faceRight=true;
+    private Animator playerAnim;
+
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        playerAnim=GetComponent<Animator>();
+
         rb2D = GetComponent<Rigidbody2D>();
     }
 
@@ -28,21 +33,41 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position,checkRadius, whatIsGround);
+        playerAnim.SetBool("IsGrounded",isGrounded);
         movePlayer();
     }
 
     private void getInput()
     {
         movementHorizontal = Input.GetAxis("Horizontal");
+        playerAnim.SetFloat("Movement",Mathf.Abs(movementHorizontal));
 
         if (Input.GetAxis("Jump") > 0 && isGrounded)
         {
             rb2D.velocity = Vector2.up * jumpForce;
+        }
+        playerAnim.SetFloat("Jump",rb2D.velocity.y);
+
+        if (movementHorizontal > 0 && !faceRight)
+        {
+            FlipChar();
+        }
+        else if (movementHorizontal < 0 && faceRight)
+        {
+            FlipChar();
         }
     }
 
     private void movePlayer()
     {
         rb2D.velocity = new Vector2(movementHorizontal * speed, rb2D.velocity.y);
+    }
+
+    private void FlipChar()
+    {
+        faceRight = !faceRight;
+        Vector3 flip = transform.localScale;
+        flip.x *= -1;
+        transform.localScale = flip;
     }
 }
